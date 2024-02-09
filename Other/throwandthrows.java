825038e2c6c0f7da5420859245b3db34fed769a7
Sukumar755 <https://github.com/Sukumar755/Java.git>
2024-02-09 09:38:56 -0600

    .

--- Other/throwandthrows.java
+++ Other/throwandthrows.java
@@ -3,30 +3,16 @@ package Other;
 import java.util.Scanner;
 
 public class throwandthrows {
-
-    static void methodMultiplication(int number){
-        for(int i =1;i<=10;i++){
-
-            System.out.println(number+" X "+i+" = "+ (number*i));
-        }
-    }
-
-
-
-    public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-        System.out.print("Give me a number to print the multiplication table: ");
-        int num = input.nextInt();
-
-        methodMultiplication(num);
-
-
-
+public static void main(String[] args){
 
 
 
 
 
 }
+
+
+
+
 }
+
