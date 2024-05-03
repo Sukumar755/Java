e31e936d30dd29b33da3e1753f7c45c834ed4c58
Raj <118765541+Sukumar755@users.noreply.github.com>
2024-05-03 09:38:23 -0500

    Delete Other directory

--- Other/FileoutputEx.java
+++
@@ -1,2 +0,0 @@
-package Other;public class FileoutputEx {
-}
--- Other/JAVA8.java
+++
@@ -1,9 +0,0 @@
-package Other;
-import java.util.*;
-public class Lambda {
-    public static void main(String[] args) {
-        () -> {
-            System.out.println("hello");
-        }
-    }
-}
